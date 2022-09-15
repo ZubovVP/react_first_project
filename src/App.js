3fca488a5b1eebf7bf37296d5b92e65f56ee6c6c
Vitaly Zubov <Zubov.VP@yandex.ru>
2022-09-15 23:40:44 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+const App = () => {
+    return (
+        <div>
+            <div>One div</div>
+            <div className="App">
+                <ul>
+                    <li>css</li>
+                    <li>html</li>
+                    <li>js</li>
+                    <li>react</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default App;
