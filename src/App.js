1032e32670b2fe168c84e1ede5d01b7bbe5ead5a
Vitaly Zubov <Zubov.VP@yandex.ru>
2022-09-18 22:06:03 +0300

    Вёрстка grib

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
 import './App.css';
-import Technologies from "./Technologies";
-import Header from "./Header";
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src='logo192.png'/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Setting</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src='logo192.png'/>
+                </div>
+                <div>ava + description</div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 }
