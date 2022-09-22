35912113e96c4768769d82908e2949064c3bf0ca
Vitaly Zubov <Zubov.VP@yandex.ru>
2022-09-22 23:30:49 +0300

    Add props for Post

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -11,8 +11,8 @@ const MyPosts = () => {
                 <button>Add post</button>
             </div>
             <div>
-                <Post/>
-                <Post/>
+                <Post message='Hi. how are you?' likeCount='10'/>
+                <Post message="It's my first post" likeCount='15'/>
             </div>
         </div>
     );
