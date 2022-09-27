2b463cc1ac73053d4712a76d7a1c89de49ada226
Vitaly Zubov <Zubov.VP@yandex.ru>
2022-09-27 22:41:37 +0300

    Add url for dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,30 +1,33 @@
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 <div className={s.dialog}>
-                    Duke
+                    <NavLink to="/dialogs/1">Duke</NavLink>
                 </div>
                 <div className={s.dialog + ' ' + s.active}>
-                    Alex
+                    <NavLink to="/dialogs/2">Alex</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Kate
+                    <NavLink to="/dialogs/3">Kate</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    River
+                    <NavLink to="/dialogs/4">River</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Vitaly
+                    <NavLink to="/dialogs/5">Vitaly</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/6">Valera</NavLink>
                 </div>
             </div>
             <div className={s.messages}>
                 <div className={s.message}>Hi</div>
                 <div className={s.message}>How are you></div>
                 <div className={s.message}>Yo</div>
-
             </div>
         </div>
     );
