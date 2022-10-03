d55a0e307f22ff3185e092156dcbafff72a71119
Vitaly Zubov <Zubov.VP@yandex.ru>
2022-10-03 21:43:33 +0300

    Refactoring (выносим данные в index.js)

--- src/App.js
+++ src/App.js
@@ -12,27 +12,7 @@ import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 
-const App = () => {
-    let posts = [
-        {id: 1, text: 'Hi. how are you?', likesCount: 10},
-        {id: 2, text: 'It\'s my first post', likesCount: 15},
-    ];
-
-    let dialogs = [
-        {id: 1, name: 'Duke'},
-        {id: 2, name: 'Alex'},
-        {id: 3, name: 'Kate'},
-        {id: 4, name: 'River'},
-        {id: 5, name: 'Vitaly'},
-        {id: 6, name: 'Valera'}
-    ];
-
-    let messages = [
-        {id: 1, text: 'Hi'},
-        {id: 2, text: 'How are you?'},
-        {id: 3, text: 'Yo'},
-    ];
-
+const App = (props) => {
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
@@ -40,8 +20,9 @@ const App = () => {
                 <Navbar/>
                 <div className='app-wrapper-content'>
                     <Routes>
-                        <Route path="/dialogs/*" element={<Dialogs dialogs={dialogs} messages={messages}/>}/>
-                        <Route path="/profile" element={<Profile posts={posts}/>}/>
+                        <Route path="/dialogs/*"
+                               element={<Dialogs dialogs={props.dialogs} messages={props.messages}/>}/>
+                        <Route path="/profile" element={<Profile posts={props.posts}/>}/>
                         <Route path="/news" element={<News/>}/>
                         <Route path="/music" element={<Music/>}/>
                         <Route path="/settings" element={<Settings/>}/>
--- src/index.js
+++ src/index.js
@@ -4,11 +4,31 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let posts = [
+    {id: 1, text: 'Hi. how are you?', likesCount: 10},
+    {id: 2, text: 'It\'s my first post', likesCount: 15},
+];
+
+let dialogs = [
+    {id: 1, name: 'Duke'},
+    {id: 2, name: 'Alex'},
+    {id: 3, name: 'Kate'},
+    {id: 4, name: 'River'},
+    {id: 5, name: 'Vitaly'},
+    {id: 6, name: 'Valera'}
+];
+
+let messages = [
+    {id: 1, text: 'Hi'},
+    {id: 2, text: 'How are you?'},
+    {id: 3, text: 'Yo'},
+];
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App posts={posts} dialogs={dialogs} messages={messages}/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
