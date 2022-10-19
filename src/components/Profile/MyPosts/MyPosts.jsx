f87e5e3fd7ab47f7ec9c450b6e65bfe24d55bef5
Vitaly Zubov <Zubov.VP@yandex.ru>
2022-10-19 22:27:40 +0300

    add action creator

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import './MyPosts.module.css';
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
+import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/state";
 
 const MyPosts = (props) => {
 
@@ -9,15 +10,12 @@ const MyPosts = (props) => {
     let newPostElement = React.createRef();
 
     let addPost = () => {
-        props.dispatch({type: 'ADD-POST'});
+        props.dispatch(addPostActionCreator());
     }
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        let action = {
-            type: 'UPDATE-NEW-POST-TEXT',
-            newText: text
-        };
+        let action = updateNewPostTextActionCreator(text);
         props.dispatch(action);
     };
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -95,3 +95,16 @@ let store = {
 }
 export default store;
 window.store = store;
+
+export const addPostActionCreator = () => {
+    return {
+        type: 'ADD-POST'
+    };
+};
+
+export const updateNewPostTextActionCreator = (text) => {
+    return {
+        type: 'UPDATE-NEW-POST-TEXT',
+        newText: text
+    };
+}
