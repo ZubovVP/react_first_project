664fdb1808ea7156be66122749b1869ae9f21511
Vitaly Zubov <Zubov.VP@yandex.ru>
2022-10-24 23:44:59 +0300

    add reducer for sidebar

--- src/redux/sidebarReducer.js
+++ src/redux/sidebarReducer.js
@@ -1,7 +1,29 @@
- const sidebarReducer = (state, action) => {
+const ADD_NEW_FRIEND = 'ADD-FRIEND';
+const UPDATE_FRIEND = 'UPDATE-FRIEND';
 
+const sidebarReducer = (state, action) => {
+    switch (action.type) {
+        case ADD_NEW_FRIEND:
+            let name = state.name;
+            state.friends.push({id: 9, name: name});
+            return state;
 
-    return state;
+        case UPDATE_FRIEND:
+            let username = state.name;
+            let id = state.id;
+            for (let i = 0; i < state.length; i++) {
+                if (state.friends[i].id === id) {
+                    state.friends[i].name = username;
+                    return state;
+                }
+            }
+            return state;
+
+        default:
+            return state;
+    }
 }
 
+export const addSidebarFriendCreator = () => ({type: ADD_NEW_FRIEND});
+export const updateSidebarFriendCreator = () => ({type: UPDATE_FRIEND});
 export default sidebarReducer;
