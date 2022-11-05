01bd39f576c32cd07dfda2ca6de97e8ee312f4fa
Vitaly Zubov <Zubov.VP@yandex.ru>
2022-11-05 22:44:01 +0300

    Add connect, mapStateToProps for friends

--- src/components/Friends/FriendsContainter.jsx
+++ src/components/Friends/FriendsContainter.jsx
@@ -1,23 +1,17 @@
 import React from "react";
 import './Friends.module.css';
-import StoreContext from "../../StoreContext";
 import DialogItem from "../Dialogs/DialogItem/DialogItem";
 import Friends from "./Friends";
+import {connect} from "react-redux";
 
-const FriendsContainer = () => {
+let mapStateToProps = (state) => {
+    let dialogsElements = state.sidebar.friends.map(friend => <DialogItem name={friend.name}
+                                                                          id={friend.id}/>);
+    return {
+        dialogsElements: dialogsElements
+    }
+};
 
-    return (
-        <StoreContext.Consumer>
-            {
-                (store) => {
-                    let state = store.getState();
-                    let dialogsElements = state.sidebar.friends.map(friend => <DialogItem name={friend.name} id={friend.id}/>);
-
-                    return <Friends dialogsElements={dialogsElements}/>
-                }
-            }
-        </StoreContext.Consumer>
-    );
-}
+const FriendsContainer = connect(mapStateToProps)(Friends);
 
 export default FriendsContainer;
