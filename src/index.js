e9e2c2cb5de57b5e22adda38fc562b835aa475b4
Vitaly Zubov <Zubov.VP@yandex.ru>
2022-11-06 22:22:39 +0300

    make copy for state in ProfileReducer

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import {Provider} from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-let rerenderEntireTree = () => {
     root.render(
         <React.StrictMode>
             <Provider store={store}>
@@ -16,14 +15,6 @@ let rerenderEntireTree = () => {
             </Provider>
         </React.StrictMode>
     );
-};
-
-rerenderEntireTree(store.getState());
-
-store.subscribe(() => {
-    let state = store.getState();
-    rerenderEntireTree(state);
-});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -1,30 +1,33 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
-const initialState =  {
+const initialState = {
     posts: [
         {id: 1, text: 'Hi. how are you?', likesCount: 10},
         {id: 2, text: 'It\'s my first post', likesCount: 15},
     ],
-        newPostText: 'Hello'
+    newPostText: 'Hello'
 };
 
 const profileReducer = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_POST:
+        case ADD_POST: {
             let newPost = {
                 id: 3,
                 text: state.newPostText,
                 likesCount: 0
             };
-            state.posts.push(newPost);
-            state.newPostText = '';
-            return state;
-
-        case UPDATE_NEW_POST_TEXT:
-            state.newPostText = action.newText;
-            return state;
-
+            let newState = {...state};
+            newState.posts = [...state.posts];
+            newState.posts.push(newPost);
+            newState.newPostText = '';
+            return newState;
+        }
+        case UPDATE_NEW_POST_TEXT: {
+            let newState = {...state};
+            newState.newPostText = action.newText;
+            return newState;
+        }
         default:
             return state;
     }
