e99284e33ee7701ee4c574fc6f1d9547c015971b
Vitaly Zubov <Zubov.VP@yandex.ru>
2022-11-09 23:19:06 +0300

    Add friendItem

--- src/components/Friends/FriendsContainter.jsx
+++ src/components/Friends/FriendsContainter.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import './Friends.module.css';
-import DialogItem from "../Dialogs/DialogItem/DialogItem";
 import Friends from "./Friends";
 import {connect} from "react-redux";
+import FriendItem from "./FriendItem/FriendItem";
 
 let mapStateToProps = (state) => {
-    let dialogsElements = state.sidebar.friends.map(friend => <DialogItem name={friend.name}
-                                                                          id={friend.id}/>);
+    let dialogsElements = state.sidebar.friends.map(friend => <FriendItem name={friend.name}
+                                                                          key={friend.id}/>);
     return {
         dialogsElements: dialogsElements
     }
