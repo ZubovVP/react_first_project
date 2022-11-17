e438336ac28dae2b8092444cadc3e16a20515026
Vitaly Zubov <Zubov.VP@yandex.ru>
2022-11-17 23:43:58 +0300

    add button (getUsers)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,13 +4,18 @@ import axios from "axios";
 import userPhoto from '../../../src/asserts/images/user.webp'
 
 let Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items);
-        });
+    let getUsers = () =>
+    {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items);
+            });
+        }
+    };
 
-   }
     return <div>
+        <button onClick={getUsers}>Get users</button>
+
         {
             props.users.map(u => <div key={u.id}>
                 <span>
@@ -23,7 +28,7 @@ let Users = (props) => {
                             : <button onClick={() => props.follow(u.id)}>Follow</button>}
                         </div>
                 </span>
-                <span>
+                    <span>
                     <span>
                         <div>{u.name}</div>
                         <div>{u.status}</div>
