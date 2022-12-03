529aa338e1c507eb64b0291622a970195d5cacf3
Vitaly Zubov <Zubov.VP@yandex.ru>
2022-12-03 23:14:49 +0300

    Create HeaderContainer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,13 @@
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
     return <header className={s.header}>
         <img src='./../../logo192.png'/>
+        <div className={s.loginBlock}>
+            <NavLink to={"/login"}>Login</NavLink>
+
+        </div>
     </header>
 }
 
